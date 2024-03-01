5bd6fa900b99712e3df9886d8c4b23a2922e8331
Onur Aktaş <aktsonr1903@gmail.com>
2024-03-02 00:38:39 +0300

    [Feature]: Redis Cache Config

--- src/main/java/com/redis/RedisExample/RedisExampleApplication.java
+++ src/main/java/com/redis/RedisExample/RedisExampleApplication.java
@@ -1,9 +1,12 @@
 package com.redis.RedisExample;
 
+import com.redis.RedisExample.config.CacheConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(value = CacheConfig.class)
 public class RedisExampleApplication {
 
 	public static void main(String[] args) {
